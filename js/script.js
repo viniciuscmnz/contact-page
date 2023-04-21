55b73335241bb061dddcaaae8c062b4d8c8ca870
viniciuscmnz <viniciuscassiomnz@gmail.com>
2023-04-21 15:29:27 -0300

    first step

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var email = document.querySelector('input');
+var mensagem = document.querySelector('textarea');
+var assunto = document.getElementById('assuntoid')
+
+email.addEventListener('focus',()=>{
+    email.style.borderColor = "#76b4a2";
+});
+
+email.addEventListener('blur',()=>{
+    email.style.borderColor = "#ccc";
+});
+
+mensagem.addEventListener('focus',()=>{
+    mensagem.style.borderColor = "#76b4a2";
+});
+
+mensagem.addEventListener('blur',()=>{
+    mensagem.style.borderColor = "#ccc";
+});
+
+assunto.addEventListener('focus',()=>{
+    assunto.style.borderColor = "#76b4a2";
+});
+
+assunto.addEventListener('blur',()=>{
+    assunto.style.borderColor = "#ccc";
+});
+
+
+
+
+
